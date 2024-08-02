3a5a34e3e7a8bc49b690411c3577b4f5f513c139
budi3312101075 <budiprayoga5103@gmail.com>
2024-08-02 19:40:27 +0700

    login page bug

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,13 +3,9 @@ import React from "react";
 const Login = () => {
   return (
     <div className="bg-primary w-screen h-screen flex py-3 lg:px-0 px-10">
-      <div className="lg:w-1/2 w-full h-full py-16">
-        <div className="flex flex-col bg-secondary max-h-max max-w-xl mx-auto rounded-xl px-7 h-full">
-          <img
-            src="./arff-logo.png"
-            alt="logo"
-            className="w-28 mx-auto mb-5 mt-10"
-          />
+      <div className="lg:w-1/2 w-full h-full py-28 2xl:py-56">
+        <div className="flex flex-col bg-secondary max-h-max max-w-xl mx-auto rounded-xl px-7 h-full justify-center xl:h-96 xl:-mt-14 ">
+          <img src="./arff-logo.png" alt="logo" className="w-28 mx-auto mb-5" />
           <p className="text-white ml-1 mb-1">masukan username anda :</p>
           <input
             className="mb-5 pl-2 h-10 rounded-xl bg-slate-200 text-black outline-none"
@@ -22,7 +18,7 @@ const Login = () => {
             type="password"
             placeholder="password"
           />
-          <div className="w-full text-center px-20">
+          <div className="w-full text-center px-20 ">
             <button className="bg-red-600 text-white rounded-xl w-full mx-auto mt-7 py-1">
               Masuk
             </button>
