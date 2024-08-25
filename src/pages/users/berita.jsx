2c2c5341b7a4a106292c796e844a6e9471d51ae4
budi3312101075 <budiprayoga5103@gmail.com>
2024-08-25 20:12:38 +0700

    fixed berita

--- src/pages/users/berita.jsx
+++ src/pages/users/berita.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const Berita = () => {
   const [berita, setBerita] = useState([]);
 
-  useEffect(() => {
-    const fetchBerita = async () => {
-      try {
-        const response = await axios.get("/news"); // Sesuaikan endpoint
-        setBerita(response.data);
-      } catch (error) {
-        console.error("Gagal mengambil data berita:", error);
-      }
-    };
+  const fetchBerita = async () => {
+    try {
+      const response = await axios.get("/news"); // Sesuaikan endpoint
+      setBerita(response.data);
+    } catch (error) {
+      console.error("Gagal mengambil data berita:", error);
+    }
+  };
 
+  useEffect(() => {
     fetchBerita();
   }, []);
 
@@ -22,6 +23,17 @@ const Berita = () => {
   const truncateText = (text, maxLength) => {
     if (text.length <= maxLength) return text;
     return text.substring(0, maxLength) + "...";
+  };
+
+  const onDeleted = async (data) => {
+    try {
+      await axios.delete(`/news/${data}`);
+      toast.success("Berhasil menghapus berita");
+      fetchBerita();
+    } catch (error) {
+      toast.error("Gagal menghapus berita");
+      console.error(error);
+    }
   };
 
   return (
@@ -44,25 +56,31 @@ const Berita = () => {
                 className="h-full"
                 src={
                   /<img[^>]+src="([^">]+)"/g.exec(item.content)?.[1] ||
-                  "https://via.placeholder.com/150" // Placeholder image
+                  "https://mgmall.s3.amazonaws.com/img/062023/390bed03e54f6440416f0568f61a82b563176996.jpg" // Placeholder image
                 }
                 alt={item.title}
               />
             </figure>
             <div className="card-body">
               <h1 className="card-title text-lg ">
-                {truncateText(item.title, 7)}
+                {truncateText(item.title, 10)}
               </h1>
               <p className="text-xs">
-                {truncateText(item.content.replace(/<[^>]+>/g, ""), 7)}
+                {truncateText(item.content.replace(/<[^>]+>/g, ""), 20)}
               </p>
-              <div className="card-actions justify-end">
+              <div className="flex gap-1 ">
                 <Link
                   to={`/DetailBerita/${item.uuid}`}
-                  className="bg-black text-white px-4 py-2 rounded-xl w-full text-xs text-center"
+                  className="bg-secondary text-white px-4 py-2 rounded-xl w-full text-xs text-center"
                 >
                   Lihat
                 </Link>
+                <button
+                  className="bg-red-500 text-white px-3 py-2 rounded-xl w-full text-xs text-center"
+                  onClick={() => onDeleted(item.uuid)}
+                >
+                  Hapus
+                </button>
               </div>
             </div>
           </div>
