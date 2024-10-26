696e10ec648fd8b1cd94f7de8efd4772be9b3922
budi3312101075 <budiprayoga5103@gmail.com>
2024-10-26 10:59:45 +0700

    add dashboard

--- src/pages/users/dashboard.jsx
+++ src/pages/users/dashboard.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div className="bg-primary h-full w-full text-black px-10 py-14 ">
-      <h1>djwodjowdjowjod</h1>
+      <div className="text-2xl bg-secondary h-52 rounded-xl p-5">
+        Selamat Datang Di PKP-PK Hang Nadim Batam
+      </div>
     </div>
   );
 };
